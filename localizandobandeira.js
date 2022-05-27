7d6d837477b3ccd8d68a1fae79a0273afee31aa7
João Barros <105664722+rabros@users.noreply.github.com>
2022-05-26 21:22:11 -0300

    Create localizandobandeira.js

---
+++ localizandobandeira.js
@@ -0,0 +1,7 @@
+const bandeira = [
+  ['Brasil',['Verde', 'Amarelo', 'Azul', 'Branco']],
+  ['Canadá',['Vermelho', 'Branco']],
+  ['México',['Verde', 'Vermelho', 'Branco']],
+  ['EUA',['Azul', 'Branco', 'Vermelho']],
+];
+console.log(bandeira[0][1][1]);
